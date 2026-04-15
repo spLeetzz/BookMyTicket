fe530aeec5050284755b0186b914a1816e9452e2
spLeetzz <nubsplitzz@gmail.com>
2026-04-16 01:20:48 +0530

    nnnn

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,7 @@
 import { validateEnv } from "./config/env.config.js";
 import app from "./server.js";
 import { createServer } from "node:http";
+console.log(process.env);
 validateEnv(); // load env + validate they exist, must be at top 
 const server = createServer(app);
 server.listen(process.env.PORT ?? 3000, () => {
